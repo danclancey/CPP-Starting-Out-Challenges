e5696c73d30b448e8d8e084c932b737d3bedf652
Sif33r <dan.clancey@gmail.com>
2024-04-16 19:19:28 +0000

    add challenge

--- ch04/01_minimumMaximum.cpp
+++ ch04/01_minimumMaximum.cpp
@@ -2,3 +2,28 @@
   Write a program that asks the user to enter two different integers. The program should use the conditional operator 
   to determine which number is the smaller and which is the larger.
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int num1, num2;
+
+  // Prompt user for two integers.
+  cout << "Enter the first integer: ";
+  cin >> num1;
+  cout << "Enter the second integer: ";
+  cin >> num2;
+
+  // Determine which number is smaller/larger
+  int smaller = (num1 < num2) ? num1 : num2;
+  int larger = (num1 > num2) ? num1 : num2;
+
+  // Display results
+  cout << "The smaller number is: " << smaller << endl;
+  cout << "The larger number is: " << larger << endl;
+
+  return 0;
+}
