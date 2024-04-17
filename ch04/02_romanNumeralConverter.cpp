cc1d9812666092d2431d3100414c6d228876a509
Sif33r <dan.clancey@gmail.com>
2024-04-17 18:28:54 +0000

    Add challenges

--- ch04/02_romanNumeralConverter.cpp
+++ ch04/02_romanNumeralConverter.cpp
@@ -4,3 +4,70 @@
 
   Input Validation: Decide how the program should handle an input that is less than 1 or greater than 10.
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int number;
+  
+  // Welcome Message
+  cout << "Welcome to the Roman Numeral Converter!\n";
+  cout << "---------------------------------------\n\n";
+  cout << "This program converts a number within the range of 1-10 to a Roman Numeral\n\n";
+
+  cout << "Please enter a number between 1 and 10: ";
+  cin >> number;
+  
+  // Input validation
+  if (number < 1 || number > 10) 
+  {
+    cout << "Invalid input! Exiting..\n";
+    return 1;
+  }
+
+  // Display Roman Numeral equivalent of input
+  cout << " - Roman numeral equivalent: ";
+  switch(number)
+  {
+    case 1:
+      cout << "I";
+      break;
+    case 2:
+      cout << "II";
+      break;
+    case 3:
+      cout << "III";
+      break;
+    case 4:
+      cout << "IV";
+      break;
+    case 5:
+      cout << "V";
+      break;
+    case 6:
+      cout << "VI";
+      break;
+    case 7:
+      cout << "VII";
+      break;
+    case 8:
+      cout << "VIII";
+      break;
+    case 9:
+      cout << "IX";
+      break;
+    case 10:
+      cout << "X";
+      break;
+    default: 
+      cout << "Invalid Input..";
+  }
+  
+  cout << endl;
+
+  return 0;
+
+}
--- ch04/03_magicDates.cpp
+++ ch04/03_magicDates.cpp
@@ -11,3 +11,44 @@
 
   Input Validation: Think about what legal values the program should accept for month and day.
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  // Declare variables
+  int month, day,  year;
+
+  // Welcome message
+  cout << "Welcome to the Magic Date Checker!\n";
+  cout << "----------------------------------\n\n";
+  cout << "This program determines whether a date is a magic date based on the\n"
+       << "month, day, and year entered.\n\n";
+
+  // Prompt user for month, day and year.
+  cout << "Enter a month (1-12): ";
+  cin >> month;
+  cout << "Enter a day (1-31): ";
+  cin >> day;
+  cout << "Enter a 2 digit year: ";
+  cin >> year;
+
+  // Check if input is Valid 
+  if (month < 1 || month > 12 || day < 1 || day > 31 || year < 0)
+  {
+    cout << "Invalid Input! Exiting..\n";
+  }
+
+  if (month * day == year)
+  {
+    cout << "The date " << month << "/" << day << "/" << year << " is a magic date!\n";
+  }
+  else 
+  {
+    cout << "The date " << month << "/" << day << "/" << year << " is not a magic date\n";
+  }
+
+  return 0;
+}
