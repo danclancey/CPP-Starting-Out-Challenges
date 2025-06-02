14d7cc135ea0152b9794fdc8e926e3379746912f
Dan Clancey <dan.clancey@gmail.com>
2025-06-02 17:06:21 -0400

    add challenge

--- ch12/16_pigLatin.cpp
+++ ch12/16_pigLatin.cpp
@@ -5,3 +5,50 @@ remove the first letter and place that letter at the end of the word. Then you a
 English: I SLEPT MOST OF THE NIGHT
 Pig Latin: IAY LEPTSAY OSTMAY FOAY HETAY IGHTNAY
 */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string convertTopigLatin(const string& word) {
+    if (word.empty()) {
+        return "";
+    }
+
+    if (word.length() == 1) {
+        return word + "AY";
+    }
+
+    char firstLetter = word[0];
+    string restOfWord = word.substr(1);
+
+    return restOfWord + firstLetter + "AY";
+}
+
+int main() {
+    string sentence;
+    string pigLatinSentence;
+
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+
+    stringstream ss(sentence);
+    string word;
+    bool firstWordProcessed = false;
+
+    while (ss >> word) {
+        if (firstWordProcessed) {
+            pigLatinSentence += " ";
+        }
+
+        pigLatinSentence += convertTopigLatin(word);
+        firstWordProcessed = true;
+    }
+
+    cout << "PIg Latin: " << pigLatinSentence << endl;
+
+    return 0;
+
+}
