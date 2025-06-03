a45c9d9cfe336b7bbf76dcca7f76e0cae0113fb8
Dan Clancey <dan.clancey@gmail.com>
2025-06-02 22:50:23 -0400

    add challenge

--- ch12/18_userName.cpp
+++ ch12/18_userName.cpp
@@ -6,3 +6,34 @@ name of the user is gcm, then the program prints
 
 when it is executed.
 */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    const char* usernameEnv = nullptr;
+
+    if (usernameEnv == nullptr) {
+        usernameEnv = getenv("USER");
+    }
+
+    if (usernameEnv == nullptr) {
+        usernameEnv = getenv("LOGNAME");
+    }
+
+    if(usernameEnv ==nullptr) {
+        usernameEnv = getenv ("USERNAME");
+    }
+
+    if (usernameEnv != nullptr) {
+        string username(usernameEnv);
+        cout << "Hello, " << username << endl;
+    } else {
+        cout << "Hello, unknown user! (could not determine specific login name)\n";
+    }
+
+    return 0;
+}
